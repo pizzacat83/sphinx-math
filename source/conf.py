7650c714e2a1a5218c9b9150fea51bc1bca195e3
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-07-17 01:23:56 +0900

    add math domain

--- source/conf.py
+++ source/conf.py
@@ -60,3 +60,16 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+from sphinxcontrib.domaintools import custom_domain
+
+def setup(app):
+    app.add_domain(custom_domain('MathDomain',
+        name  = 'm',
+        label = "Mathematics",
+
+        elements = dict(
+            define = dict(),
+            theorem = dict(),
+        )))
