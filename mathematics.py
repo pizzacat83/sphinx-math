c2d82693229ca357ef52d255db5d3f87e5083ca4
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-07-30 11:16:21 +0900

    implement directive proof

--- mathematics.py
+++ mathematics.py
@@ -1,7 +1,8 @@
-from docutils.nodes import Text
+from docutils.nodes import Admonition, Element, Text
 from docutils.parsers.rst import directives
 from sphinx import addnodes
 from sphinx.domains import ObjType
+from sphinx.locale import admonitionlabels
 from sphinx.roles import XRefRole
 from sphinx.util import ws_re
 from sphinxcontrib.domaintools import CustomDomain, GenericObject
@@ -68,9 +69,48 @@ class Definition(MathematicsObject):
 class Proposition(MathematicsObject):
     prefix = '命題'
 
+
+class MathematicsAdmonition(Admonition, Element):
+    label = ''
+    name = ''
+
+    @classmethod
+    def register(cls, app):
+        admonitionlabels[cls.name] = cls.label
+
+        def visit_node(self, node):
+            self.visit_admonition(node, cls.name)
+
+        def depart_node(self, node):
+            self.depart_admonition(node)
+
+        # TODO: support latex, plaintext
+        app.add_node(cls,
+                     html=(visit_node, depart_node))
+
+
+class ProofAdmonition(MathematicsAdmonition):
+    label = '証明'
+    name = 'proof'
+
+
+class Proof(directives.admonitions.BaseAdmonition):
+    node_class = ProofAdmonition
+
+
+def visit_proof_node(self, node):
+    self.visit_admonition(node, 'proof')
+
+
+def depart_proof_node(self, node):
+    self.depart_admonition(node)
+
+
 def setup(app):
     MathematicsDomain.add_object('thm', Theorem)
     MathematicsDomain.add_object('def', Definition)
     MathematicsDomain.add_object('prop', Proposition)
+    MathematicsDomain.add_directive('pf', Proof)
+    ProofAdmonition.register(app)
     app.add_domain(MathematicsDomain)
 
