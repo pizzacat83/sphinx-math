858cd7e695018081cf10f20cd19436ddc432d3cb
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-08-05 19:02:07 +0900

    update

--- source/conf.py
+++ source/conf.py
@@ -11,7 +11,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
+import sys
+print(sys.path)
 # sys.path.insert(0, os.path.abspath('.'))
 
 
